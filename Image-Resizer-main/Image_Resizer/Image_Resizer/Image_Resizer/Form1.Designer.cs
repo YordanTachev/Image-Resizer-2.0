cbd955ff9c4510e86b7728adfb26c46eaead0108
Йордан Тачев 2201681014 <134278152+YordanTachev@users.noreply.github.com>
2023-11-08 19:14:42 +0200

    Add files via upload

---
+++ Image-Resizer-main/Image_Resizer/Image_Resizer/Image_Resizer/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Image_Resizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSelect = new System.Windows.Forms.TextBox();
+            this.txtSave = new System.Windows.Forms.TextBox();
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnResize = new System.Windows.Forms.Button();
+            this.btnFSave = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select Image";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 65);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Save Image";
+            // 
+            // txtSelect
+            // 
+            this.txtSelect.Location = new System.Drawing.Point(108, 18);
+            this.txtSelect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSelect.Name = "txtSelect";
+            this.txtSelect.Size = new System.Drawing.Size(468, 22);
+            this.txtSelect.TabIndex = 2;
+            // 
+            // txtSave
+            // 
+            this.txtSave.Location = new System.Drawing.Point(108, 62);
+            this.txtSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSave.Name = "txtSave";
+            this.txtSave.Size = new System.Drawing.Size(468, 22);
+            this.txtSave.TabIndex = 3;
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(601, 18);
+            this.btnSelect.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(100, 28);
+            this.btnSelect.TabIndex = 4;
+            this.btnSelect.Text = "...";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(601, 58);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(100, 28);
+            this.btnSave.TabIndex = 5;
+            this.btnSave.Text = "...";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnResize
+            // 
+            this.btnResize.Location = new System.Drawing.Point(245, 106);
+            this.btnResize.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnResize.Name = "btnResize";
+            this.btnResize.Size = new System.Drawing.Size(104, 32);
+            this.btnResize.TabIndex = 8;
+            this.btnResize.Text = "Resize";
+            this.btnResize.UseVisualStyleBackColor = true;
+            this.btnResize.Click += new System.EventHandler(this.btnResize_Click);
+            // 
+            // btnFSave
+            // 
+            this.btnFSave.Location = new System.Drawing.Point(392, 106);
+            this.btnFSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnFSave.Name = "btnFSave";
+            this.btnFSave.Size = new System.Drawing.Size(104, 32);
+            this.btnFSave.TabIndex = 9;
+            this.btnFSave.Text = "Save";
+            this.btnFSave.UseVisualStyleBackColor = true;
+            this.btnFSave.Click += new System.EventHandler(this.btnFSave_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 113);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(69, 22);
+            this.textBox1.TabIndex = 10;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 117);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Scale Factor";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(104, 94);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 16);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "INT";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(584, 117);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 214);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnFSave);
+            this.Controls.Add(this.btnResize);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSelect);
+            this.Controls.Add(this.txtSave);
+            this.Controls.Add(this.txtSelect);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSelect;
+        private System.Windows.Forms.TextBox txtSave;
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnResize;
+        private System.Windows.Forms.Button btnFSave;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Image-Resizer-main/Image_Resizer/Image_Resizer/Image_Resizer/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Image_Resizer
+{
+    public partial class Form1 : Form
+    {
+        Bitmap orgImage;
+
+        int newWidth;
+        int newHeight;
+
+        Bitmap newImage;
+        int sF;
+
+        void ThreadWOrker1(int from, int to)
+        {
+
+            for (int x = from; x < to; x++)
+            {
+                for (int y = 0; y < newHeight; y++)
+                {
+                    int red = 0, green = 0, blue = 0, pixelCount = 0;
+
+                    for (int newX = 0; newX < orgImage.Width / 2; newX++)
+                    {
+
+                        for (int newY = 0; newY < orgImage.Height; newY++)
+                        {
+                            int finalX = x * orgImage.Width / newWidth;
+                            int finalY = y * orgImage.Height / newHeight;
+
+                            Color color = orgImage.GetPixel(finalX, finalY);
+                            red += color.R;
+                            green += color.G;
+                            blue += color.B;
+                            pixelCount++;
+                        }
+                    }
+
+                    Color averageColor = Color.FromArgb(red / pixelCount, green / pixelCount, blue / pixelCount);
+                    newImage.SetPixel(x, y, averageColor);
+                }
+            }
+        }
+        void ThreadWOrker2(int from, int to)
+        {
+
+            for (int x = from; x < to; x++)
+            {
+                for (int y = 0; y < newHeight; y++)
+                {
+                    int red = 0, green = 0, blue = 0, pixelCount = 0;
+
+                    for (int newX = orgImage.Width / 2; newX < orgImage.Width; newX++)
+                    {
+
+                        for (int newY = 0; newY < orgImage.Height; newY++)
+                        {
+                            int finalX = x * orgImage.Width / newWidth;
+                            int finalY = y * orgImage.Height / newHeight;
+
+                            Color color = orgImage.GetPixel(finalX, finalY);
+                            red += color.R;
+                            green += color.G;
+                            blue += color.B;
+                            pixelCount++;
+                        }
+                    }
+
+                    Color averageColor = Color.FromArgb(red / pixelCount, green / pixelCount, blue / pixelCount);
+                    newImage.SetPixel(x, y, averageColor);
+                }
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "images | *.png;*.jpg;*.jpeg;*.gif";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                txtSelect.Text = ofd.FileName;
+                orgImage = new Bitmap(ofd.FileName);
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (fbd.ShowDialog() == DialogResult.OK)
+                txtSave.Text = fbd.SelectedPath;
+        }
+
+        private void btnResize_Click(object sender, EventArgs e)
+        {
+
+
+            if (orgImage != null && !string.IsNullOrEmpty(textBox1.Text))
+            {
+                sF = Int32.Parse(textBox1.Text);
+                Thread t1 = new Thread(p => ThreadWOrker1(0, newWidth / 2));
+                Thread t2 = new Thread(p => ThreadWOrker2(newWidth / 2, newWidth));
+                t1.Start();
+                t1.Join();
+                t2.Start();
+                t2.Join();
+                MessageBox.Show("Image Resized");
+            }
+            else
+            {
+                MessageBox.Show("Please select the image and provide the resize value first.");
+            }
+
+
+
+            newWidth = orgImage.Width * sF;
+            newHeight = orgImage.Height * sF;
+            newImage = new Bitmap(newWidth, newHeight);
+
+         
+        }
+
+        private void btnFSave_Click(object sender, EventArgs e)
+        {
+            if (newImage != null && !string.IsNullOrEmpty(txtSave.Text))
+            {
+                newImage.Save(txtSave.Text + "\\rImage.png");
+                MessageBox.Show("Image Saved");
+            }
+            else
+            {
+                MessageBox.Show("Please resize the image and provide the save path first.");
+            }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
